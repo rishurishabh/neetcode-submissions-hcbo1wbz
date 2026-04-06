cad39548354451b2445785372a988b279ecade26
Rishabh Kumar <33497918+rishurishabh@users.noreply.github.com>
2026-04-06 16:19:51 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        // nums.sort((a,b) => a-b);
+
+        // for(let i = 1; i < nums.length; i++) {
+        //     if(nums[i] === nums[i-1]) {
+        //         return true;
+        //     }
+        // }
+
+        let set = new Set();
+
+        for(let num of nums) {
+            if(set.has(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
