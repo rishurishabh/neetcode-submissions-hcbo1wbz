3e8e991f06f788706d526ef445d1ed785ae1ec67
Rishabh Kumar <33497918+rishurishabh@users.noreply.github.com>
2026-04-06 16:28:01 +0530

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        // let len = nums.length;
+        // let res = new Array(2*len);
+
+        // for(let i = 0; i < len; i++) {
+        //     res[i] = nums[i];
+        //     res[len+i] = nums[i];
+        // }
+        // return res;
+
+        // return [...nums, ...nums];
+
+        return nums.concat(nums)
+    }
+}
